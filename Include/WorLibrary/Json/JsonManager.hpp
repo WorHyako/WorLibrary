02adb3188b4b2e4fc0b599a27386e1fec3af1a2a
WorHyako <worhyako@gmail.com>
2024-07-05 11:28:23 +0200

    change WorJson API

--- Include/WorLibrary/Json/JsonManager.hpp
+++ Include/WorLibrary/Json/JsonManager.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <fstream>
 #include <string>
 
 #include "nlohmann/json.hpp"
@@ -10,123 +9,26 @@ namespace Wor::Json {
     /**
      * @brief
      *
+     * @param filePath
+     *
+     * @param jsonString
+     *
+     * @return
+     *
      * @author WorHyako
      */
-    class JsonManager final {
-    public:
-        /**
-         * @brief Enum describes current file state
-         *
-         * @author WorHyako
-         */
-        enum class FileStatus
-                : std::uint8_t {
-            /**
-             * File didn't checked
-             */
-            ZeroCheck,
-            /**
-             * File don't exist
-             */
-            Unreachable,
-            /**
-             * File is busy by other process
-             */
-            Busy,
-            /**
-             * File ready to read/write operation
-             */
-            Ready
-        };
+    [[nodiscard]]
+    bool tryToSaveFile(const std::string &filePath, const std::string &jsonString) noexcept;
 
-        /**
-         * Ctor
-         */
-        JsonManager();
-
-        /**
-         * @brief   Check file to destination and accessible file.
-         *          <p/>
-         *          Result will wrote to internal variable.
-         *          <p/>
-         *          Also file path will saved to internal variable if successful
-         *
-         * @param   filePath Absolute or relative file path
-         *
-         * @return  file status
-         */
-        [[nodiscard]]
-        FileStatus tryToFindFile(const std::string &&filePath, bool createFile = true) noexcept;
-
-        /**
-         * @brief
-         *
-         * @param jsonString
-         * @param scope
-         *
-         * @return
-         */
-        [[nodiscard]] bool
-        tryToSaveFile(const std::string &jsonString, const std::string &scope = {}) noexcept;
-
-        /**
-         * @brief
-         *
-         * @param scopeName
-         *
-         * @return
-         */
-        [[nodiscard]]
-        nlohmann::json tryToLoadFile(const std::string &scopeName = {}) noexcept;
-
-    private:
-        /**
-         * Path to current status
-         */
-        std::string _filePath;
-
-        /**
-         * Current file status
-         */
-        FileStatus _fileStatus;
-
-        /**
-         * @brief   Just check file to existing
-         *
-         * @param   filePath absolute or relative file path
-         *
-         * @return  checking result
-         */
-        [[nodiscard]]
-        static bool isFileExist(const std::string &filePath) noexcept;
-
-        /**
-         * @brief   Small method just to make code more comfortable
-         *
-         * @return  is file ready
-         */
-        [[nodiscard]]
-        inline bool isFileReady() const noexcept;
-
-        /**
-         * @brief
-         *
-         * @param   filePath
-         *
-         * @return
-         */
-        [[nodiscard]]
-        static inline bool createFile(const std::string &filePath) noexcept;
-
-    public:
-#pragma region Accessors
-
-        [[nodiscard]]
-        std::string getFileName() const noexcept;
-
-        [[nodiscard]]
-        FileStatus getFileStatus() const noexcept;
-
-#pragma endregion Accessors
-    };
+    /**
+     * @brief
+     *
+     * @param filePath
+     *
+     * @return
+     *
+     * @author WorHyako
+     */
+    [[nodiscard]]
+    nlohmann::json tryToLoadFile(const std::string &filePath) noexcept;
 }
--- Src/Json/JsonManager.cpp
+++ Src/Json/JsonManager.cpp
@@ -1,49 +1,47 @@
 #include "WorLibrary/Json/JsonManager.hpp"
 
-#include <sys/stat.h>
+#include <fstream>
 
-using namespace Wor::Json;
+using namespace Wor;
 
-JsonManager::JsonManager()
-        : _fileStatus(FileStatus::ZeroCheck),
-          _filePath("none") {
-}
-
-JsonManager::FileStatus JsonManager::tryToFindFile(const std::string&& filePath, bool create) noexcept {
-    if (create) {
-        _fileStatus = createFile(filePath)
-                      ? FileStatus::Ready
-                      : FileStatus::Unreachable;
-    } else {
-        _fileStatus = isFileExist(filePath)
-                      ? FileStatus::Ready
-                      : FileStatus::Unreachable;
+namespace {
+    /**
+     * @brief
+     *
+     * @param filePath
+     *
+     * @return
+     *
+     * @author WorHyako
+     */
+    [[nodiscard]]
+    bool checkFile(const std::string &filePath) noexcept {
+        std::ofstream file;
+        try {
+            file.open(filePath, std::ios::app | std::ios::in);
+            file.close();
+        } catch (const std::ofstream::failure &e) {
+            std::printf("Exception opening/reading file\n");
+            return false;
+        }
+        return true;
     }
-    if (_fileStatus == FileStatus::Ready) {
-        _filePath = filePath;
-    }
-    return _fileStatus;
 }
 
-bool JsonManager::isFileExist(const std::string& filePath) noexcept {
-    struct stat buffer{};
-    return stat(filePath.c_str(), &buffer) == 0;
-}
-
-bool JsonManager::tryToSaveFile(const std::string& jsonString, const std::string& scope) noexcept {
-    if (!isFileReady()) {
+bool Json::tryToSaveFile(const std::string &filePath, const std::string &jsonString) noexcept {
+    if (!checkFile(filePath)) {
         return false;
     }
     nlohmann::json fileContent;
     {
-        std::ifstream file(_filePath);
+        std::ifstream file(filePath);
         auto fileEmpty = file.peek() == std::ifstream::traits_type::eof();
         if (file.is_open() && !fileEmpty) {
             try {
                 fileContent = nlohmann::json::parse(file);
             }
-            catch (...) {
-                /// Nothing happened. Just forgive this scope.
+            catch (const nlohmann::json::exception& e) {
+                std::printf("Fail to parse %s file\n Error: %s", filePath.c_str(), e.what());
             }
             if (!fileContent.is_object()) {
                 fileContent.clear();
@@ -51,59 +49,32 @@ bool JsonManager::tryToSaveFile(const std::string& jsonString, const std::string
         }
         file.close();
     }
-    if (scope.empty()) {
-        fileContent.push_back(nlohmann::json::parse(jsonString));
-    } else {
-        fileContent[scope] = nlohmann::json::parse(jsonString);
-    }
+    fileContent.push_back(nlohmann::json::parse(jsonString));
     {
-        std::ofstream file(_filePath);
+        std::ofstream file(filePath);
         file << std::setw(4) << fileContent.dump(4);
         file.close();
     }
     return true;
 }
 
-nlohmann::json JsonManager::tryToLoadFile(const std::string& scopeName) noexcept {
-    if (!isFileReady()) {
+nlohmann::json Json::tryToLoadFile(const std::string &filePath) noexcept {
+    if (!checkFile(filePath)) {
         return false;
     }
     nlohmann::json fileContent;
-    std::ifstream file(_filePath);
-    auto fileEmpty = file.peek() == std::ifstream::traits_type::eof();
-    if (file.is_open() && !fileEmpty) {
-        try {
-            fileContent = nlohmann::json::parse(file);
-        }
-        catch (...) {
-            /// Nothing happened. Just forgive this scope.
-        }
+    std::ifstream file(filePath);
+    const auto fileEmpty = file.peek() == std::ifstream::traits_type::eof();
+    if (!file.is_open() && fileEmpty) {
+        return {};
     }
+    try {
+        fileContent = nlohmann::json::parse(file);
+    }
+    catch (...) {
+        std::printf("Fail to parse %s file\n", filePath.c_str());
+    }
+
     file.close();
-    return scopeName.empty()
-           ? fileContent
-           : fileContent[scopeName];
+    return fileContent;
 }
-
-inline bool JsonManager::isFileReady() const noexcept {
-    return _fileStatus == FileStatus::Ready;
-}
-
-bool JsonManager::createFile(const std::string& filePath) noexcept {
-    std::ofstream file;
-    file.open(filePath, std::ios::app | std::ios::in);
-    file.close();
-    return isFileExist(filePath);
-}
-
-#pragma region Accessors
-
-std::string JsonManager::getFileName() const noexcept {
-    return _filePath;
-}
-
-JsonManager::FileStatus JsonManager::getFileStatus() const noexcept {
-    return _fileStatus;
-}
-
-#pragma endregion Accessors
